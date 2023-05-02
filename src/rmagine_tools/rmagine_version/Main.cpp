5ba79193a14d071c030a1c4a7519cc3185cd4f7a
Alexander Mock <amock@uos.de>
2023-05-02 13:51:26 +0200

    added rmagine version tool

---
+++ src/rmagine_tools/rmagine_version/Main.cpp
@@ -0,0 +1,12 @@
+
+#include <rmagine/util/assimp/prints.h>
+// #include <assimp/Importer.hpp>
+// #include <rmagine/map/AssimpIO.hpp>
+
+using namespace rmagine;
+
+int main(int argc, char** argv)
+{
+    std::cout << RMAGINE_VERSION << std::endl;
+    return 0;
+}
