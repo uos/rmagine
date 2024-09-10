e7f6039dfdcc21d4feb8b76b30ef9d1766fa5ac1
Alexander Mock <amock@uos.de>
2024-09-10 22:54:16 +0200

    added ouster component

---
+++ src/rmagine_ouster/src/types/ouster_sensors.cpp
@@ -0,0 +1,23 @@
+#include "rmagine/types/ouster_sensors.h"
+
+#include <jsoncpp/json/json.h>
+#include <fstream>
+
+namespace rmagine {
+
+O1DnModel o1dn_from_ouster_config(std::string filename)
+{
+  O1DnModel ret;
+
+  std::ifstream ouster_file(filename, std::ifstream::binary);
+
+  Json::Value ouster_config;
+  ouster_file >> ouster_config;
+
+  std::cout << ouster_config << std::endl;
+
+
+  return ret;
+}
+
+} // namespace rmagine
