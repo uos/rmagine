e4771e4c93f1f91f2838a2672e8b902ed5b3693e
Alexander Mock <amock@uos.de>
2025-05-14 09:38:34 +0200

    started added version functions

---
+++ src/rmagine_core/include/rmagine/version.h
@@ -0,0 +1,15 @@
+#ifndef RMAGINE_VERSION_H
+#define RMAGINE_VERSION_H
+
+#include <rmagine/types/shared_functions.h>
+
+namespace rmagine
+{
+
+size_t version_major();
+size_t version_minor();
+size_t version_patch();
+
+} // namespace rmagine
+
+#endif RMAGINE_VERSION_H
---
+++ src/rmagine_core/src/version.cpp
@@ -0,0 +1,25 @@
+#include <rmagine/version.h>
+
+namespace rmagine
+{
+
+size_t version_major()
+{
+    // TODO
+    return 0;
+}
+
+size_t version_minor()
+{
+    // TODO
+    return 0;
+}
+
+size_t version_patch()
+{
+    // TODO
+    return 0;
+}
+
+} // namespace rmagine
+
